4171cfda06615dd1fde940fea4393a931118c72c
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-04-01 14:46:12 -0400

    Update test.sh

--- mapreduce-test-python/part2/Q2/test.sh
+++ mapreduce-test-python/part2/Q2/test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-../../start.sh
+../../../start.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /part2/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /part2/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /part2/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/hdfstest2/shot_logs.csv /part2/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../../mapreduce-test-data/shot_logs.csv /part2/input/
 
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 -file mapper.py -mapper mapper.py \
@@ -15,4 +15,4 @@ echo "--------------------------- Question 2: Best zones for Curry, Harden, Lebr
 
 /usr/local/hadoop/bin/hdfs dfs -rm -r /part2/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /part2/output/
-../../stop.sh
+../../../stop.sh
