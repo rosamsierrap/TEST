9b8d2985857881ea3abaf859f5f66f93bdddc0ec
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-04-02 09:54:51 -0400

    Update color_mapper.py

--- mapreduce-test-python/part1/color_mapper.py
+++ mapreduce-test-python/part1/color_mapper.py
@@ -8,6 +8,23 @@ for line in csv.reader(sys.stdin, quotechar='"'):
 
     # If color data is found
     if (color):
+      if color in ("GY","GRAY", "GRY", "GREY", "Gray"):
+        color = "GRAY"
+      elif color in ("BLK","BK","BLACK"):
+        color = "BLACK" 
+      elif color in ("WH","WHITE", "WHT", "WHI"):
+        color = "WHITE" 
+      elif color in ("SILVE","SLVR","SIL"):
+        color = "SILVER"
+      elif color in ("GREEN","GRN","GREN"):
+        color = "GREEN" 
+      elif color in ("YW","YELLO"):
+        color = "YELLOW" 
+      elif color in ("BLUE", "BLU"):
+        color = "BLUE" 
+      elif color in ("RED", "RD"):
+        color = "RED" 
+        
       print('{}\t{}'.format(color, 1))
 
 
