02c8e0b0e705e3f88d2e71f194c39b3f70283c1e
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-04-02 15:51:33 -0400

    Update mapper.py

--- mapreduce-test-python/bonus/part1/mapper.py
+++ mapreduce-test-python/bonus/part1/mapper.py
@@ -5,15 +5,15 @@ street_codes=['34510','10030','34050']
 #Filter Vehicle colors values found in the range of data, there can be more values options
 black_colors= ["BLK","BK","BLACK","B K","BLAK","BLCK","BC"]
 
-for line in sys.stdin:
+for line in csv.reader(sys.stdin, quotechar='"'):
 
     #Remove commas and splits
-    line=line.strip(',').split(',')
-    line_length=len(line)
+    #line=line.strip(',').split(',')
+    #line_length=len(line)
 
     # Checking if the length is 43 is a way to ensure the data point has all 43 attributes
-    if line_length==43:
-        continue
+    #if line_length==43:
+     #   continue
 
     if line[33] in black_colors:
         prob_ticket='Yes'
