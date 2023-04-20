1cb28fa0961cc3a806c166aa981fe2fc9334b946
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-04-20 19:38:28 -0400

    Update mapper_b.py

--- mapreduce-test-python/part2/Q2/mapper_b.py
+++ mapreduce-test-python/part2/Q2/mapper_b.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python
 # ---- coding:utf-8 ----
 import sys
-p = {}
-for line in sys.stdin:
-    try:
-        info = line.split(',')
-        shooter = (info[15]+info[16]).strip('"')
-        hit = info[14]
-        def_dist = float(info[18])
-        time = float(info[9])
-        dist = float(info[12])
-        if shooter not in p:
-            p[shooter] = []
-        if hit == 'made':
-            p[shooter].append((dist,def_dist,time))
-    except:
-        pass
+from pyspark import SparkContext, SparkConf
 
-for shooter,info in p.items():
-    print(shooter+'\t'+str(info))
+conf = SparkConf().setAppName("PART1")
+sc = SparkContext(conf=conf)
+
+textFile = sc.textFile(sys.argv[1]) #data
+
+info = textFile.flatMap(lambda line: line.split("\n")) #lines
+
+ShotDistDefdistTime= info.map(lambda line: (line.split(",")[15]+line.split(",")[16].strip('"'),  #shooter
+                                            (float(line.split(",")[12]),  #dist
+                                             float(line.split(",")[18]), #def_dist
+                                             float(line.split(",")[9])))) #time
+
+made = ShotDistDefdistTime.filter(lambda pair: pair[1][0] == 'made') #hit==made
+
+made_byshooter = made.groupByKey()
+
+p = made_byshooter.map(lambda pair: (pair[0], list(pair[1]))).collectAsMap()
+
+output = sc.parallelize([shooter+'\t'+str(info) for shooter,info in p.items()])
+output.saveAsTextFile("output.txt")
+
+sc.stop()
+                      
