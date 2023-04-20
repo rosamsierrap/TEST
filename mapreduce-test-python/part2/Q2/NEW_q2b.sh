e939884d103d1bde8e5c691d228afdc7862046e9
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-04-20 18:44:49 -0400

    Create NEW_q2b.sh

---
+++ mapreduce-test-python/part2/Q2/NEW_q2b.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+../../../start.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /q2/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /q2/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p  /q2/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../../mapreduce-test-data/shot_logs.csv /q2/input/
+
+echo "Copy and startup complete"
+
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
+-file ./mapper_b.py -mapper ./mapper_b.py \
+-file ./reducer_b.py -reducer ./reducer_b.py \
+-input /q2/input/* -output /q2/output/
+
+echo "RESULTS"
+/usr/local/hadoop/bin/hdfs dfs -cat /q2/output/*
