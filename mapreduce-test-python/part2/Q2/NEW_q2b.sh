310a45632c566057d4eddcc98cd99b71b2be4312
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-04-22 11:25:22 -0400

    Update NEW_q2b.sh

--- mapreduce-test-python/part2/Q2/NEW_q2b.sh
+++ mapreduce-test-python/part2/Q2/NEW_q2b.sh
@@ -1,16 +1,7 @@
-#!/bin/sh
-../../../start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /q2/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /q2/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p  /q2/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../../mapreduce-test-data/shot_logs.csv /q2/input/
-
-echo "Copy and startup complete"
-
-/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file mapper_b.py -mapper mapper_b.py \
--file reducer_b.py -reducer reducer_b.py \
--input /q2/input/* -output /q2/output/
-
-echo "RESULTS"
-/usr/local/hadoop/bin/hdfs dfs -cat /q2/output/*
+#!/bin/bash
+source ../../env.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /line-counter/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /line-counter/output
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /line-counter/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../test-data/test.txt /line-counter/input/
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./counter.py hdfs://$SPARK_MASTER:9000/line-counter/input/
